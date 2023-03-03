41446e9c6350e23f34308f115c8c878bc727e3a2
Yuliya Romashko <ramashkay@gmail.com>
2023-03-04 01:53:08 +0300

    Calculations changed, validator added

--- src/main/java/by/it_academy/fitnessstudio/service/RecipeService.java
+++ src/main/java/by/it_academy/fitnessstudio/service/RecipeService.java
@@ -1,72 +1,70 @@
 package by.it_academy.fitnessstudio.service;
 
 import by.it_academy.fitnessstudio.core.dto.*;
-import by.it_academy.fitnessstudio.core.dto.product.Product;
+import by.it_academy.fitnessstudio.core.dto.error.ErrorCode;
 import by.it_academy.fitnessstudio.core.exception.InvalidInputServiceMultiException;
 import by.it_academy.fitnessstudio.core.exception.InvalidInputServiceSingleException;
 import by.it_academy.fitnessstudio.entity.IngredientEntity;
-import by.it_academy.fitnessstudio.entity.ProductEntity;
 import by.it_academy.fitnessstudio.entity.RecipeEntity;
 import by.it_academy.fitnessstudio.repositories.api.RecipeEntityRepository;
+import by.it_academy.fitnessstudio.service.api.IProductService;
 import by.it_academy.fitnessstudio.service.api.IRecipeService;
+import by.it_academy.fitnessstudio.validator.api.IValidator;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class RecipeService implements IRecipeService {
     private final RecipeEntityRepository repository;
-    private final ProductService productService;
+    private final IProductService productService;
     private final ConversionService conversionService;
+    private final IValidator<RecipeCreate> validator;
 
-    public RecipeService(RecipeEntityRepository repository, ProductService productService, ConversionService conversionService) {
+    public RecipeService(RecipeEntityRepository repository, IProductService productService, ConversionService conversionService, IValidator<RecipeCreate> validator) {
         this.repository = repository;
         this.productService = productService;
         this.conversionService = conversionService;
+        this.validator = validator;
     }
 
     @Override
     public void save(RecipeCreate recipeCreate) {
         if(recipeCreate == null) {
-            throw new InvalidInputServiceSingleException("Recipe information not submitted for update");
+            throw new InvalidInputServiceSingleException("Recipe information not submitted for update", ErrorCode.ERROR);
         }
 
-        validate(recipeCreate);
+        validator.validate(recipeCreate);
         checkUniqueTitle(recipeCreate);
         checkProductExist(recipeCreate);
+
         String title = recipeCreate.getTitle();
 
-        //без конвертера
         List<IngredientEntity> comp = recipeCreate.getComposition().stream()
                 .map(ingredient -> new IngredientEntity(
                         productService.getEntity(ingredient.getProduct().getUuid()),
                         ingredient.getWeight()))
                 .toList();
 
+        LocalDateTime dtCreate = LocalDateTime.now();
         RecipeEntity recipeEntity = RecipeEntity.RecipeEntityBuilder.create()
                 .setUuid(UUID.randomUUID())
-                .setDtCreate(LocalDateTime.now())
-                .setDtUpdate(LocalDateTime.now())
+                .setDtCreate(dtCreate)
+                .setDtUpdate(dtCreate)
                 .setTitle(title)
                 .setComposition(comp).build();
         repository.save(recipeEntity);
-
-       //используя конвертер для ингредиента
-       /*List<IngredientEntity> comp = recipeCreate.getComposition().stream()
-                .map(ingredient -> conversionService.convert(ingredient, IngredientEntity.class)).toList();
-       repository.save(new RecipeEntity(title, comp));*/
-
-        //используя конвертер для рецепта
-        //repository.save(conversionService.convert(recipeCreate, RecipeEntity.class));
     }
 
     @Override
     public OnePage<Recipe> getRecipesPage(Integer page, Integer size) {
-        InvalidInputServiceMultiException multiException = new InvalidInputServiceMultiException("structured_error");
+        InvalidInputServiceMultiException multiException = new InvalidInputServiceMultiException(ErrorCode.STRUCTURED_ERROR);
 
         if(page == null || page < 0) {
             multiException.addSuppressed(new InvalidInputServiceMultiException("Invalid field value. Field must be 0 or greater", "page"));
@@ -82,7 +80,7 @@ public class RecipeService implements IRecipeService {
 
         Page<RecipeEntity> all = repository.findAll(PageRequest.of(page, size));
 
-        List<Recipe> recipes = all.get().map(entity -> createRecipe(entity)).toList();
+        List<Recipe> recipes = all.get().map(entity -> calculateRecipe(entity)).toList();
 
         int number = all.getNumber();
         int sizePage = all.getSize();
@@ -97,8 +95,8 @@ public class RecipeService implements IRecipeService {
     }
 
     @Override
-    public void update(UUID uuid, Long dtUpdate, RecipeCreate recipeCreate) {
-        InvalidInputServiceMultiException multiException = new InvalidInputServiceMultiException("Invalid input");
+    public void update(UUID uuid, Long dtUpdate, RecipeCreate recipeCreate) {//д/приходить LDT во второй параметр
+        InvalidInputServiceMultiException multiException = new InvalidInputServiceMultiException(ErrorCode.STRUCTURED_ERROR);
 
         if(uuid == null) {
             multiException.addSuppressed(new InvalidInputServiceMultiException("UUID not entered", "uuid"));
@@ -117,19 +115,18 @@ public class RecipeService implements IRecipeService {
             multiException.addSuppressed(new InvalidInputServiceMultiException("Recipe information not submitted for update", "recipe"));
         }
 
-        //TODO или сначала валидация рецепта, если он не налл, а потом уже выкидывать все искл-я??
         if(multiException.getSuppressed().length != 0) {
             throw multiException;
         }
 
-        validate(recipeCreate);
+        validator.validate(recipeCreate);
 
         String recipeCreateTitle = recipeCreate.getTitle();
 
         Optional<RecipeEntity> recipeById = repository.findById(uuid);
 
         if(recipeById.isEmpty()) {
-            throw new InvalidInputServiceSingleException("Recipe with this uuid doesn't exist");
+            throw new InvalidInputServiceSingleException("Recipe with this uuid doesn't exist", ErrorCode.ERROR);
         }
 
         checkProductExist(recipeCreate);
@@ -153,58 +150,18 @@ public class RecipeService implements IRecipeService {
             entity.setComposition(comp);
             repository.save(entity);
         } else {
-            throw new InvalidInputServiceSingleException("Recipe with this version doesn't exist");
-        }
-    }
-    
-    private void validate(RecipeCreate recipeCreate) {
-        InvalidInputServiceMultiException multiException = new InvalidInputServiceMultiException("Invalid input");
-        String title = recipeCreate.getTitle();
-        
-        if(title == null || title.isEmpty() || title.isBlank()) {
-             multiException.addSuppressed(new InvalidInputServiceMultiException("Title not entered", "title"));
-        }
-
-        List<Ingredient> composition = recipeCreate.getComposition();
-
-        if (composition == null) {
-            multiException.addSuppressed(new InvalidInputServiceMultiException("Composition not entered", "composition"));
-            throw multiException;
-        }
-
-        for (Ingredient ingredient : composition) {
-            if (ingredient == null) {
-                multiException.addSuppressed(new InvalidInputServiceMultiException("Ingredient not entered", "ingredient"));
-            } else {
-                Product product = ingredient.getProduct();
-                if(product == null) {
-                    multiException.addSuppressed(new InvalidInputServiceMultiException("Product not entered", "product"));
-                } else {
-                    UUID uuid = product.getUuid();
-                    if(uuid == null) {
-                        multiException.addSuppressed(new InvalidInputServiceMultiException("UUID not entered", "uuid"));
-                    }
-                }
-                int weight = ingredient.getWeight();
-                if(weight <= 0) {
-                    multiException.addSuppressed(new InvalidInputServiceMultiException("Incorrect weight", "weight"));
-                }
-            }
-        }
-
-        if(multiException.getSuppressed().length != 0) {
-            throw multiException;
+            throw new InvalidInputServiceSingleException("Recipe with this version doesn't exist", ErrorCode.ERROR);
         }
     }
 
     private void checkUniqueTitle(RecipeCreate recipeCreate) {
         if(repository.existsByTitle(recipeCreate.getTitle())) {
-            throw new InvalidInputServiceSingleException("Not a unique recipe name");
+            throw new InvalidInputServiceSingleException("Not a unique recipe name", ErrorCode.ERROR);
         }
     }
 
     private void checkProductExist(RecipeCreate recipeCreate) {
-        InvalidInputServiceMultiException multiException = new InvalidInputServiceMultiException("structured_error");
+        InvalidInputServiceMultiException multiException = new InvalidInputServiceMultiException(ErrorCode.STRUCTURED_ERROR);
 
         List<Ingredient> composition = recipeCreate.getComposition();
         for (Ingredient ingredient : composition) {
@@ -220,14 +177,14 @@ public class RecipeService implements IRecipeService {
         }
     }
 
-    private Recipe createRecipe (RecipeEntity entity) {
-        int totalWeight = 0;
-        int totalCalories = 0;
-        double totalProteins = 0;
-        double totalFats = 0;
-        double totalCarbohydrates = 0;
+    private Recipe calculateRecipe(RecipeEntity entity) {
+        BigInteger totalWeight = BigInteger.valueOf(0);
+        BigInteger totalCalories = BigInteger.valueOf(0);
+        BigDecimal totalProteins = BigDecimal.valueOf(0.0);
+        BigDecimal totalFats = BigDecimal.valueOf(0.0);
+        BigDecimal totalCarbohydrates = BigDecimal.valueOf(0.0);
 
-        Recipe.RecipeBuilder recipeBuilder = Recipe.RecipeBuilder.create();//TODO: или лучше вынести в поле??
+        Recipe.RecipeBuilder recipeBuilder = Recipe.RecipeBuilder.create();
 
         recipeBuilder.setUuid(entity.getUuid())
                 .setDtCreate(conversionService.convert(entity.getDtCreate(), Long.class))
@@ -240,74 +197,29 @@ public class RecipeService implements IRecipeService {
 
         for (IngredientEntity ingredientEntity : composition) {
 
-            IngredientCalculated ingredientCalculated = createIngredient(ingredientEntity);
+            IngredientCalculated ingredientCalculated = calculateIngredient(ingredientEntity);
 
             compositionDTO.add(ingredientCalculated);
 
-            totalWeight += ingredientCalculated.getWeight();
-            totalCalories += ingredientCalculated.getCalories();
-            totalProteins += ingredientCalculated.getProteins();
-            totalFats += ingredientCalculated.getFats();
-            totalCarbohydrates += ingredientCalculated.getCarbohydrates();
-
+            totalWeight = totalWeight.add(BigInteger.valueOf(ingredientCalculated.getWeight()));
+            totalCalories = totalCalories.add(BigInteger.valueOf(ingredientCalculated.getCalories()));
+            totalProteins = totalProteins.add(BigDecimal.valueOf(ingredientCalculated.getProteins()));
+            totalFats = totalFats.add(BigDecimal.valueOf(ingredientCalculated.getFats()));
+            totalCarbohydrates = totalCarbohydrates.add(BigDecimal.valueOf(ingredientCalculated.getCarbohydrates()));
         }
 
+        int scale = 2;
         return recipeBuilder.setComposition(compositionDTO)
-                .setWeight(totalWeight)
-                .setCalories(totalCalories)
-                .setProteins(totalProteins)
-                .setFats(totalFats)
-                .setCarbohydrates(totalCarbohydrates)
+                .setWeight(totalWeight.intValue())
+                .setCalories(totalCalories.intValue())
+                .setProteins(totalProteins.setScale(scale, RoundingMode.HALF_UP).doubleValue())
+                .setFats(totalFats.setScale(scale, RoundingMode.HALF_UP).doubleValue())
+                .setCarbohydrates(totalCarbohydrates.setScale(scale, RoundingMode.HALF_UP).doubleValue())
                 .build();
     }
 
-
-    private IngredientCalculated createIngredient(IngredientEntity ingredientEntity){
-        /*Product.ProductBuilder productBuilder = Product.ProductBuilder.create();
-        IngredientCalculated.IngredientCalculatedBuilder ingredientBuilder
-                = IngredientCalculated.IngredientCalculatedBuilder.create();
-
-        ProductEntity product = ingredientEntity.getProduct();
-        int productWeight = product.getWeight();
-        int productCalories = product.getCalories();
-        double productProteins = product.getProteins();
-        double productFats = product.getFats();
-        double productCarbohydrates = product.getCarbohydrates();
-        Product prodDTO = productBuilder
-                .setUuid(product.getUuid())
-                .setDtCreate(product.getDtCreate())
-                .setDtUpdate(product.getDtUpdate())
-                .setTitle(product.getTitle())
-                .setWeight(product.getWeight())
-                .setCalories(productCalories)
-                .setProteins(productProteins)
-                .setFats(productFats)
-                .setCarbohydrates(productCarbohydrates)
-                .build();
-
-        int weight = ingredientEntity.getWeight();
-        int calIngr = (int) Math.round(productCalories * 1.0 / productWeight * weight);
-        double protIngr = Precision.round(productProteins / productWeight * weight, 2);
-        double fatsIngr = Precision.round(productFats / productWeight * weight, 2);
-        double carbIngr = Precision.round(productCarbohydrates / productWeight * weight, 2);
-
-        return ingredientBuilder
-                .setProduct(prodDTO).setWeight(weight)
-                .setCalories(calIngr).setProteins(protIngr)
-                .setFats(fatsIngr).setCarbohydrates(carbIngr)
-                .build();*/
+    private IngredientCalculated calculateIngredient(IngredientEntity ingredientEntity){
         return conversionService.convert(ingredientEntity, IngredientCalculated.class);
-    }
-
-    private OnePage<Recipe> createOnePage (Page<RecipeEntity> all, List<Recipe> recipes) {//TODO: builder
-        int number = all.getNumber();
-        int sizePage = all.getSize();
-        int totalPages = all.getTotalPages();
-        long totalElements = all.getTotalElements();
-        boolean first = all.isFirst();
-        int numberOfElements = all.getNumberOfElements();
-        boolean last = all.isLast();
-        return new OnePage<>(number, sizePage, totalPages, totalElements, first, numberOfElements, last, recipes);
     }
 
 }
