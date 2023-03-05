5f360c499ce801fb82aba865d70a85dd36574683
Yuliya Romashko <ramashkay@gmail.com>
2023-03-05 14:32:14 +0300

    String->LDT deserializer is created

---
+++ src/main/java/by/it_academy/fitnessstudio/converters/StringMillisToLocalDateTimeDeserializer.java
@@ -0,0 +1,16 @@
+package by.it_academy.fitnessstudio.converters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+@Component
+public class StringMillisToLocalDateTimeDeserializer implements Converter<String, LocalDateTime> {
+    @Override
+    public LocalDateTime convert(String source) {
+        Long decode = Long.decode(source);
+        return LocalDateTime.ofInstant(Instant.ofEpochMilli(decode), ZoneOffset.UTC);
+    }
+}
