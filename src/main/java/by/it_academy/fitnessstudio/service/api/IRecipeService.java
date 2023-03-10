583543cc76ac3aa998bbe0e4321dcbcf56ee421c
Yuliya Romashko <ramashkay@gmail.com>
2023-03-11 01:43:44 +0300

    Added: annotations for validation of methods' parameters IRecipeService

--- src/main/java/by/it_academy/fitnessstudio/service/api/IRecipeService.java
+++ src/main/java/by/it_academy/fitnessstudio/service/api/IRecipeService.java
@@ -3,14 +3,18 @@ package by.it_academy.fitnessstudio.service.api;
 import by.it_academy.fitnessstudio.core.dto.OnePage;
 import by.it_academy.fitnessstudio.core.dto.recipe.Recipe;
 import by.it_academy.fitnessstudio.core.dto.recipe.RecipeCreate;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface IRecipeService {
 
-    void save(RecipeCreate recipeCreate);
+    void save(@NotNull RecipeCreate recipeCreate);
 
-    OnePage<Recipe> getRecipesPage(Integer page, Integer size);
+    OnePage<Recipe> getRecipesPage(@NotNull Pageable pageable);
 
-    void update(UUID uuid, Long dtUpdate, RecipeCreate recipeCreate);
+    void update(@NotNull UUID uuid, @NotNull @Past LocalDateTime dtUpdate, @NotNull RecipeCreate recipeCreate);
 }
