ff85bd8936d413f4915dfd59a86848c0c0153c3b
Yuliya Romashko <ramashkay@gmail.com>
2023-03-11 01:50:51 +0300

    Added: annotations for validation; Fixed: parameter of date in updateRecipe()(Long->LDT)

--- src/main/java/by/it_academy/fitnessstudio/web/controllers/RecipeController.java
+++ src/main/java/by/it_academy/fitnessstudio/web/controllers/RecipeController.java
@@ -1,14 +1,20 @@
 package by.it_academy.fitnessstudio.web.controllers;
 
 import by.it_academy.fitnessstudio.core.dto.OnePage;
-import by.it_academy.fitnessstudio.core.dto.Recipe;
-import by.it_academy.fitnessstudio.core.dto.RecipeCreate;
+import by.it_academy.fitnessstudio.core.dto.recipe.Recipe;
+import by.it_academy.fitnessstudio.core.dto.recipe.RecipeCreate;
 import by.it_academy.fitnessstudio.service.api.IRecipeService;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
-
+@Validated
 @RestController
 @RequestMapping(path = "/api/v1/recipe")
 public class RecipeController {
@@ -25,14 +31,14 @@ public class RecipeController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public OnePage<Recipe> getRecipesPage(@RequestParam(name = "page") Integer page,
-                                          @RequestParam(name = "size") Integer size) {
-        //валидация параметров: здесь или на сервисе
-        return recipeService.getRecipesPage(page, size);
+    public OnePage<Recipe> getRecipesPage(@RequestParam(name = "page", defaultValue = "0") @NotNull Integer page,
+                                          @RequestParam(name = "size", defaultValue = "20") @NotNull Integer size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return recipeService.getRecipesPage(pageable);
     }
     @RequestMapping(path = "/{uuid}/dt_update/{dt_update}", method = RequestMethod.PUT)
-    public void updateRecipe(@PathVariable("uuid") UUID uuid,
-                             @PathVariable("dt_update") Long dtUpdate,
+    public void updateRecipe(@PathVariable("uuid") @NotNull UUID uuid,
+                             @PathVariable("dt_update") @NotNull @Past LocalDateTime dtUpdate,
                              @RequestBody RecipeCreate recipeCreate) {
         recipeService.update(uuid, dtUpdate, recipeCreate);
     }
