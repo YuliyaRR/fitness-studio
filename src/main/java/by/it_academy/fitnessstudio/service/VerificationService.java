bb08e6e955c8a73a1c08c468efa16ea6d49e1158
Yuliya Romashko <ramashkay@gmail.com>
2023-03-10 23:21:38 +0300

    Updated: implementation of methods, annotations for validation VerificationService

--- src/main/java/by/it_academy/fitnessstudio/service/VerificationService.java
+++ src/main/java/by/it_academy/fitnessstudio/service/VerificationService.java
@@ -2,16 +2,15 @@ package by.it_academy.fitnessstudio.service;
 
 import by.it_academy.fitnessstudio.core.dto.VerificationCode;
 import by.it_academy.fitnessstudio.core.dto.error.ErrorCode;
-import by.it_academy.fitnessstudio.core.exception.InvalidInputServiceMultiException;
-import by.it_academy.fitnessstudio.core.exception.InvalidInputServiceSingleException;
 import by.it_academy.fitnessstudio.core.exception.VerificationException;
 import by.it_academy.fitnessstudio.entity.VerificationCodeEntity;
 import by.it_academy.fitnessstudio.repositories.api.VerificationCodeEntityRepository;
 import by.it_academy.fitnessstudio.service.api.IVerificationService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
-import java.util.Optional;
-import java.util.UUID;
-
+@Validated
 public class VerificationService implements IVerificationService {
     private final VerificationCodeEntityRepository repository;
 
@@ -20,53 +19,20 @@ public class VerificationService implements IVerificationService {
     }
 
     @Override
-    public void save(VerificationCode verificationCode) {
-        if (verificationCode == null) {
-            throw new InvalidInputServiceSingleException("Verification code not entered", ErrorCode.ERROR);
-        }
-
+    public void save(@NotNull @Valid VerificationCode verificationCode) {
         repository.save(new VerificationCodeEntity(verificationCode.getCode(), verificationCode.getMail()));
     }
 
     @Override
-    public void verify(VerificationCode verificationCode) {
-        if (verificationCode == null) {
-            throw new InvalidInputServiceSingleException("Verification code not entered", ErrorCode.ERROR);
-        }
+    public void verify(@NotNull @Valid VerificationCode verificationCode) {
 
-        validateVerification(verificationCode);
+        VerificationCodeEntity verificationCodeEntity = repository.findById(verificationCode.getCode())
+                .orElseThrow(() -> new VerificationException("Invalid verification code", ErrorCode.ERROR));
 
-        Optional<VerificationCodeEntity> byCode = repository.findById(verificationCode.getCode());
-
-        if (byCode.isEmpty()) {
-            throw new InvalidInputServiceSingleException("This code was not found in the database", ErrorCode.ERROR);
-        }
-
-        VerificationCodeEntity verificationCodeEntity = byCode.get();
-
-        if (verificationCode.getCode().equals(verificationCodeEntity.getCode())) {
+        if (verificationCode.getMail().equals(verificationCodeEntity.getEmail())) {
             repository.delete(verificationCodeEntity);
         } else {
-            throw new VerificationException("Invalid verification code", ErrorCode.ERROR);
-        }
-
-    }
-
-    private void validateVerification(VerificationCode verificationCode) {
-        InvalidInputServiceMultiException multiException = new InvalidInputServiceMultiException(ErrorCode.STRUCTURED_ERROR);
-        UUID code = verificationCode.getCode();
-        String mail = verificationCode.getMail();
-
-        if(code == null) {
-            multiException.addSuppressed(new InvalidInputServiceMultiException("Code not entered", "code"));
-        }
-
-        if(mail == null || mail.isBlank() || mail.isEmpty()){
-            multiException.addSuppressed(new InvalidInputServiceMultiException("Email not entered", "mail"));
-        }
-
-        if(multiException.getSuppressed().length != 0) {
-            throw multiException;
+            throw new VerificationException("Invalid email", ErrorCode.ERROR);
         }
     }
 }
