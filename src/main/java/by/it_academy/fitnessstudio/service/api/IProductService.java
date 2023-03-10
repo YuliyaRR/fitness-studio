197479c233484beb9b9765545566a523d4c287cb
Yuliya Romashko <ramashkay@gmail.com>
2023-03-11 00:31:56 +0300

    Added: annotations for validation of methods' parameters IProductService

--- src/main/java/by/it_academy/fitnessstudio/service/api/IProductService.java
+++ src/main/java/by/it_academy/fitnessstudio/service/api/IProductService.java
@@ -4,18 +4,21 @@ import by.it_academy.fitnessstudio.core.dto.OnePage;
 import by.it_academy.fitnessstudio.core.dto.product.Product;
 import by.it_academy.fitnessstudio.core.dto.product.ProductCreate;
 import by.it_academy.fitnessstudio.entity.ProductEntity;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface IProductService {
 
-    void create(ProductCreate productCreate);
+    void create(@NotNull @Valid ProductCreate productCreate);
 
-    OnePage<Product> getProductsPage(Integer page, Integer size);
+    OnePage<Product> getProductsPage(@NotNull Pageable pageable);
 
-    void updateProduct(UUID uuid, Long dtUpdate, ProductCreate productCreate);
+    void updateProduct(@NotNull UUID uuid, @NotNull @Past LocalDateTime dtUpdate, @NotNull @Valid ProductCreate productCreate);
 
-    Product get(UUID uuid);
-
-    ProductEntity getEntity(UUID uuid);
+    ProductEntity getEntity(@NotNull UUID uuid);
 }
