25d431ff3afabc95a2bae244609f4e4c1d847042
Yuliya Romashko <ramashkay@gmail.com>
2023-03-16 00:44:33 +0300

    Updated: JwtFilter

--- product-service/src/main/java/by/it_academy/product/web/controllers/filter/JwtFilter.java
+++ product-service/src/main/java/by/it_academy/product/web/controllers/filter/JwtFilter.java
@@ -1,5 +1,6 @@
 package by.it_academy.product.web.controllers.filter;
 
+import by.it_academy.product.core.dto.user.UserToken;
 import by.it_academy.product.web.controllers.utils.JwtTokenUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -7,16 +8,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
 
 import static org.apache.logging.log4j.util.Strings.isEmpty;
 
@@ -42,49 +39,11 @@ public class JwtFilter extends OncePerRequestFilter {
             return;
         }
 
-        UserDetails userDetails = new UserDetails() {
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return null;
-            }
-
-            @Override
-            public String getPassword() {
-                return null;
-            }
-
-            @Override
-            public String getUsername() {
-                return JwtTokenUtil.getUsername(token);
-            }
-
-            @Override
-            public boolean isAccountNonExpired() {
-                return false;
-            }
-
-            @Override
-            public boolean isAccountNonLocked() {
-                return false;
-            }
-
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return false;
-            }
-
-            @Override
-            public boolean isEnabled() {
-                return false;
-            }
-        };
+        UserToken userToken = JwtTokenUtil.getUserToken(token);
 
         UsernamePasswordAuthenticationToken
                 authentication = new UsernamePasswordAuthenticationToken(
-                userDetails, null,
-                userDetails == null ?
-                        List.of() : userDetails.getAuthorities()
-        );
+                userToken, null,userToken.getAuthorities());
 
         authentication.setDetails(
                 new WebAuthenticationDetailsSource().buildDetails(request)
--- user-service/src/main/java/by/it_academy/user/web/controllers/filter/JwtFilter.java
+++ user-service/src/main/java/by/it_academy/user/web/controllers/filter/JwtFilter.java
@@ -1,6 +1,7 @@
 package by.it_academy.user.web.controllers.filter;
 
 import by.it_academy.user.core.dto.user.User;
+import by.it_academy.user.core.dto.user.UserToken;
 import by.it_academy.user.service.api.IAuthenticationService;
 import by.it_academy.user.web.controllers.utils.JwtTokenUtil;
 import jakarta.servlet.FilterChain;
@@ -9,15 +10,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.apache.logging.log4j.util.Strings.isEmpty;
 
@@ -49,14 +47,14 @@ public class JwtFilter extends OncePerRequestFilter {
             return;
         }
 
-        User user = authenticationService.get(JwtTokenUtil.getUsername(token));
-        List<SimpleGrantedAuthority> list = new ArrayList<>();
-        list.add(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()));
+        User user = authenticationService.get(JwtTokenUtil.getMail(token));
+        UserToken userToken = new UserToken(user.getMail(), user.getRole().name(), user.getFio());
+
 
         UsernamePasswordAuthenticationToken
                 authentication = new UsernamePasswordAuthenticationToken(
-                user, null,
-                list);
+                userToken, null,
+                userToken.getAuthorities());
 
         authentication.setDetails(
                 new WebAuthenticationDetailsSource().buildDetails(request)
