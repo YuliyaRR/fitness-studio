e45ee7f4783127a5a5eb6581ca4a29df260a0757
Yuliya Romashko <ramashkay@gmail.com>
2023-03-16 00:41:37 +0300

    Created: UserToken

---
+++ product-service/src/main/java/by/it_academy/product/core/dto/user/UserToken.java
@@ -0,0 +1,81 @@
+package by.it_academy.product.core.dto.user;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class UserToken implements UserDetails {
+    private String mail;
+    private String role;
+     private String fio;
+
+    public UserToken(String mail, String role, String fio) {
+        this.mail = mail;
+        this.role = role;
+        this.fio = fio;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<SimpleGrantedAuthority> authority = new ArrayList<>();
+        authority.add(new SimpleGrantedAuthority("ROLE_" + role));
+        return authority;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return fio;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
