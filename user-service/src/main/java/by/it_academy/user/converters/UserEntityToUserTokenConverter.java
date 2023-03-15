eba93e4c6ccf280e57cad7c45e9896160f083e3a
Yuliya Romashko <ramashkay@gmail.com>
2023-03-16 00:58:11 +0300

    Created: UserEntityToUserTokenConverter

---
+++ user-service/src/main/java/by/it_academy/user/converters/UserEntityToUserTokenConverter.java
@@ -0,0 +1,15 @@
+package by.it_academy.user.converters;
+
+import by.it_academy.user.core.dto.user.UserToken;
+import by.it_academy.user.entity.UserEntity;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserEntityToUserTokenConverter implements Converter<UserEntity, UserToken> {
+
+    @Override
+    public UserToken convert(UserEntity source) {
+        return new UserToken(source.getMail(), source.getRole().getRole().name(), source.getFio());
+    }
+}
