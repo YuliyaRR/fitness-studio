18b1bae10f2759af093619e7af472845524e8006
Yuliya Romashko <ramashkay@gmail.com>
2023-03-16 00:40:14 +0300

    Updated: return type

--- product-service/src/main/java/by/it_academy/product/service/UserHolder.java
+++ product-service/src/main/java/by/it_academy/product/service/UserHolder.java
@@ -1,13 +1,13 @@
 package by.it_academy.product.service;
 
-import by.it_academy.product.core.dto.user.User;
+import by.it_academy.product.core.dto.user.UserToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-
 import org.springframework.stereotype.Component;
+
 @Component
 public class UserHolder {
-    public User getUser(){
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public UserToken getUser(){
+        return (UserToken) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
 }
--- user-service/src/main/java/by/it_academy/user/service/UserHolder.java
+++ user-service/src/main/java/by/it_academy/user/service/UserHolder.java
@@ -1,12 +1,13 @@
 package by.it_academy.user.service;
 
-import by.it_academy.user.core.dto.user.User;
+import by.it_academy.user.core.dto.user.UserToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
 @Component
 public class UserHolder {
-    public User getUser(){
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    public UserToken getUser(){
+        return (UserToken) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 
 }
