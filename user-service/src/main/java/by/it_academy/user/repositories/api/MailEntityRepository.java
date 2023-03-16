c3e5d179f05a2d6b04ccbbce1fa0bd99e5c8f5ac
Yuliya Romashko <ramashkay@gmail.com>
2023-03-17 00:50:45 +0300

    Deleted: extra classes

--- user-service/src/main/java/by/it_academy/user/repositories/api/MailEntityRepository.java
+++
@@ -1,9 +0,0 @@
-package by.it_academy.user.repositories.api;
-
-import by.it_academy.user.entity.MailEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MailEntityRepository extends CrudRepository<MailEntity, String> {
-}
