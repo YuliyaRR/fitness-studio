64d5366ccf1a752b5db0f086c2bd314bbfae0d76
Yuliya Romashko <ramashkay@gmail.com>
2023-03-17 01:06:31 +0300

    Added: @EnableConfigurationProperties to UserApplication

--- user-service/src/main/java/by/it_academy/user/UserApplication.java
+++ user-service/src/main/java/by/it_academy/user/UserApplication.java
@@ -1,8 +1,11 @@
 package by.it_academy.user;
 
+import by.it_academy.user.config.JWTProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({JWTProperty.class})
 @SpringBootApplication
 public class UserApplication {
 
