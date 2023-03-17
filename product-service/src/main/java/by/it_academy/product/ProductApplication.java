ac08f9ba6a89a382d16dee043a45d47fcaa9d300
Yuliya Romashko <ramashkay@gmail.com>
2023-03-17 11:46:23 +0300

    Added: @EnableConfigurationProperties to ProductApplication

--- product-service/src/main/java/by/it_academy/product/ProductApplication.java
+++ product-service/src/main/java/by/it_academy/product/ProductApplication.java
@@ -1,8 +1,11 @@
 package by.it_academy.product;
 
+import by.it_academy.product.config.JWTProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties({JWTProperty.class})
 @SpringBootApplication
 public class ProductApplication {
 
