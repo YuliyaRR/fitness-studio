0177c1d2b752845394246760395ce3f627cb6a62
Yuliya Romashko <ramashkay@gmail.com>
2023-03-18 17:30:57 +0300

    Updated: annotations for product-service's controllers

--- product-service/src/main/java/by/it_academy/product/web/controllers/ProductController.java
+++ product-service/src/main/java/by/it_academy/product/web/controllers/ProductController.java
@@ -5,18 +5,14 @@ import by.it_academy.product.core.dto.product.Product;
 import by.it_academy.product.core.dto.product.ProductCreate;
 import by.it_academy.product.service.api.IProductService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-@Validated
 @RestController
 @RequestMapping(path = "/product")
 public class ProductController {
@@ -27,22 +23,21 @@ public class ProductController {
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    @Secured("ROLE_ADMIN")
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@Valid @RequestBody ProductCreate productCreate) {
         productService.create(productCreate);
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public OnePage<Product> getProductsPage(@RequestParam(name = "page", defaultValue = "0") @NotNull Integer page,
-                                            @RequestParam(name = "size", defaultValue = "20") @NotNull Integer size) {
+    public OnePage<Product> getProductsPage(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                            @RequestParam(name = "size", defaultValue = "20") Integer size) {
         Pageable pageable = PageRequest.of(page, size);
         return productService.getProductsPage(pageable);
     }
 
     @RequestMapping(path = "/{uuid}/dt_update/{dt_update}", method = RequestMethod.PUT)
-    public void updateProduct(@PathVariable("uuid") @NotNull UUID uuid,
-                              @PathVariable("dt_update") @NotNull @Past LocalDateTime dtUpdate,
+    public void updateProduct(@PathVariable("uuid") UUID uuid,
+                              @PathVariable("dt_update") @Past LocalDateTime dtUpdate,
                               @Valid @RequestBody ProductCreate productCreate) {
         productService.updateProduct(uuid, dtUpdate, productCreate);
     }
--- product-service/src/main/java/by/it_academy/product/web/controllers/RecipeController.java
+++ product-service/src/main/java/by/it_academy/product/web/controllers/RecipeController.java
@@ -4,17 +4,14 @@ import by.it_academy.product.core.dto.OnePage;
 import by.it_academy.product.core.dto.recipe.Recipe;
 import by.it_academy.product.core.dto.recipe.RecipeCreate;
 import by.it_academy.product.service.api.IRecipeService;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
-@Validated
 @RestController
 @RequestMapping(path = "/recipe")
 public class RecipeController {
@@ -31,14 +28,14 @@ public class RecipeController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public OnePage<Recipe> getRecipesPage(@RequestParam(name = "page", defaultValue = "0") @NotNull Integer page,
-                                          @RequestParam(name = "size", defaultValue = "20") @NotNull Integer size) {
+    public OnePage<Recipe> getRecipesPage(@RequestParam(name = "page", defaultValue = "0") Integer page,
+                                          @RequestParam(name = "size", defaultValue = "20") Integer size) {
         Pageable pageable = PageRequest.of(page, size);
         return recipeService.getRecipesPage(pageable);
     }
     @RequestMapping(path = "/{uuid}/dt_update/{dt_update}", method = RequestMethod.PUT)
-    public void updateRecipe(@PathVariable("uuid") @NotNull UUID uuid,
-                             @PathVariable("dt_update") @NotNull @Past LocalDateTime dtUpdate,
+    public void updateRecipe(@PathVariable("uuid") UUID uuid,
+                             @PathVariable("dt_update") @Past LocalDateTime dtUpdate,
                              @RequestBody RecipeCreate recipeCreate) {
         recipeService.update(uuid, dtUpdate, recipeCreate);
     }
