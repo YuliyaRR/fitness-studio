91eb8803c97101156a638b1c8b565d6e93e9820a
Yuliya Romashko <ramashkay@gmail.com>
2023-03-18 17:03:14 +0300

    Created: EmailDetailsToEntityConverter for mail-service

---
+++ mail-service/src/main/java/by/it_academy/mail/converters/EmailDetailsToEntityConverter.java
@@ -0,0 +1,19 @@
+package by.it_academy.mail.converters;
+
+import by.it_academy.mail.core.mail.EmailDetails;
+import by.it_academy.mail.entity.MailEntity;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class EmailDetailsToEntityConverter implements Converter<EmailDetails, MailEntity> {
+    @Override
+    public MailEntity convert(EmailDetails source) {
+        return new MailEntity(UUID.randomUUID(),
+                source.getMsgBody(),
+                source.getRecipient(),
+                true,1, 5);
+    }
+}
