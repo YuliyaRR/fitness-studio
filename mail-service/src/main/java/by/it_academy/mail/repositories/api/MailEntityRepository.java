2633cfbd1a8b3ea150f298c49bb45b5f3ca05ab8
Yuliya Romashko <ramashkay@gmail.com>
2023-03-29 00:48:45 +0300

    Added: methods findBy... to MailEntityRepository

--- mail-service/src/main/java/by/it_academy/mail/repositories/api/MailEntityRepository.java
+++ mail-service/src/main/java/by/it_academy/mail/repositories/api/MailEntityRepository.java
@@ -1,9 +1,15 @@
 package by.it_academy.mail.repositories.api;
 
+import by.it_academy.mail.core.mail.MailTheme;
 import by.it_academy.mail.entity.MailEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface MailEntityRepository extends CrudRepository<MailEntity, String> {
+    List<MailEntity> findByIsSendFalse();
+    Optional<MailEntity> findByEmailAndThemeAndIsSendFalse(String email, MailTheme theme);
 }
