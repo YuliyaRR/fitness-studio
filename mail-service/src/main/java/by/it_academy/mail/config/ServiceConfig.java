a7562adda51d35c7ca30ae45b67392308f1777ea
Yuliya Romashko <ramashkay@gmail.com>
2023-03-29 00:33:38 +0300

    Added: annotations for execution of asynchronous method and scheduled task to ServiceConfig(mail-service)

--- mail-service/src/main/java/by/it_academy/mail/config/ServiceConfig.java
+++ mail-service/src/main/java/by/it_academy/mail/config/ServiceConfig.java
@@ -6,14 +6,21 @@ import by.it_academy.mail.service.api.IMailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.ConversionService;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
 @Configuration
+@EnableScheduling
+@EnableAsync
 public class ServiceConfig {
     @Bean
     public IMailService mailService(MailEntityRepository mailEntityRepository,
                                     JavaMailSender javaMailSender,
-                                    ConversionService conversionService) {
-        return new MailService(mailEntityRepository, javaMailSender, conversionService);
+                                    ConversionService conversionService, TaskExecutor taskExecutor) {
+        return new MailService(mailEntityRepository, javaMailSender, conversionService, taskExecutor);
     }
 
 }
