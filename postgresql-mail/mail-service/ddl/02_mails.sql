294078899f4a3b5628bb969963542c2df93d958d
Yuliya Romashko <ramashkay@gmail.com>
2023-04-14 11:37:19 +0300

    Updated: ddl for mail-service(added column 'theme')

--- postgresql-mail/mail-service/ddl/02_mails.sql
+++ postgresql-mail/mail-service/ddl/02_mails.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS app.mails
     is_send boolean NOT NULL,
     attempt_of_sending integer NOT NULL,
     limit_of_attempts integer NOT NULL DEFAULT 5,
+    theme text NOT NULL,
     CONSTRAINT mails_pkey PRIMARY KEY (uuid)
 )
