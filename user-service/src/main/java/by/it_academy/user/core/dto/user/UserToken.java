77b8230572204039e8c153f56caf166fcb87caf2
Yuliya Romashko <ramashkay@gmail.com>
2023-04-14 11:54:07 +0300

    Changed: String role->enum UserRole for UserToken (user-service)

--- user-service/src/main/java/by/it_academy/user/core/dto/user/UserToken.java
+++ user-service/src/main/java/by/it_academy/user/core/dto/user/UserToken.java
@@ -10,14 +10,14 @@ import java.util.UUID;
 
 public class UserToken implements UserDetails {
     private String mail;
-    private String role;
+    private UserRole role;
     private String fio;
     private UUID uuid;
 
     public UserToken() {
     }
 
-    public UserToken(String mail, String role, String fio, UUID uuid) {
+    public UserToken(String mail, UserRole role, String fio, UUID uuid) {
         this.mail = mail;
         this.role = role;
         this.fio = fio;
@@ -32,7 +32,7 @@ public class UserToken implements UserDetails {
         this.mail = mail;
     }
 
-    public String getRole() {
+    public UserRole getRole() {
         return role;
     }
 
@@ -40,7 +40,7 @@ public class UserToken implements UserDetails {
         return fio;
     }
 
-    public void setRole(String role) {
+    public void setRole(UserRole role) {
         this.role = role;
     }
 
