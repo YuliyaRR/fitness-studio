4c157a326c996d185f161d9f4e434be1768ceb71
Yuliya Romashko <ramashkay@gmail.com>
2023-04-28 00:14:24 +0300

    Changed: extends JpaRepository for ProductEntityRepository

--- product-service/src/main/java/by/it_academy/product/repositories/api/ProductEntityRepository.java
+++ product-service/src/main/java/by/it_academy/product/repositories/api/ProductEntityRepository.java
@@ -1,13 +1,12 @@
 package by.it_academy.product.repositories.api;
 
 import by.it_academy.product.entity.ProductEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface ProductEntityRepository extends CrudRepository<ProductEntity, UUID>, PagingAndSortingRepository<ProductEntity, UUID> {
+public interface ProductEntityRepository extends JpaRepository<ProductEntity, UUID> {
     boolean existsByTitle(String title);
 
 }
