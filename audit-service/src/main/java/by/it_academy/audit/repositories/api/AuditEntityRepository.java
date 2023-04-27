eaf46091cad101cae4789564047105159e2c5e2a
Yuliya Romashko <ramashkay@gmail.com>
2023-04-28 00:24:47 +0300

    Changed: extends JpaRepository for AuditEntityRepository

--- audit-service/src/main/java/by/it_academy/audit/repositories/api/AuditEntityRepository.java
+++ audit-service/src/main/java/by/it_academy/audit/repositories/api/AuditEntityRepository.java
@@ -1,11 +1,10 @@
 package by.it_academy.audit.repositories.api;
 
 import by.it_academy.audit.entity.AuditEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface AuditEntityRepository extends CrudRepository<AuditEntity, UUID>, PagingAndSortingRepository<AuditEntity, UUID> {
+public interface AuditEntityRepository extends JpaRepository<AuditEntity, UUID> {
 }
