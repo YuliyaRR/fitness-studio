c8d4849ae1dd5a7eea918306cef084a9118bfce7
Yuliya Romashko <ramashkay@gmail.com>
2023-04-27 23:59:18 +0300

    Changed: CrudRepository -> JpaRepository in UserService

--- user-service/src/main/java/by/it_academy/user/repositories/api/UserEntityRepository.java
+++ user-service/src/main/java/by/it_academy/user/repositories/api/UserEntityRepository.java
@@ -1,14 +1,13 @@
 package by.it_academy.user.repositories.api;
 
 import by.it_academy.user.entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface UserEntityRepository extends CrudRepository<UserEntity, UUID>, PagingAndSortingRepository<UserEntity, UUID> {
+public interface UserEntityRepository extends JpaRepository<UserEntity, UUID>{
     boolean existsByMail(String mail);
 
 
