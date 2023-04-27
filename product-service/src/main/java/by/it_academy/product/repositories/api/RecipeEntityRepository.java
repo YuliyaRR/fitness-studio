a1a362f9b7b1bff5d9369035f353a24973dd345e
Yuliya Romashko <ramashkay@gmail.com>
2023-04-28 00:22:42 +0300

    Changed: extends JpaRepository for RecipeEntityRepository

--- product-service/src/main/java/by/it_academy/product/repositories/api/RecipeEntityRepository.java
+++ product-service/src/main/java/by/it_academy/product/repositories/api/RecipeEntityRepository.java
@@ -1,14 +1,12 @@
 package by.it_academy.product.repositories.api;
 
-
 import by.it_academy.product.entity.RecipeEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface RecipeEntityRepository extends CrudRepository<RecipeEntity, UUID>, PagingAndSortingRepository<RecipeEntity, UUID> {
+public interface RecipeEntityRepository extends JpaRepository<RecipeEntity, UUID> {
     boolean existsByTitle(String title);
 }
