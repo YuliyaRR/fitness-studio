4668c9872ebcd39d8ab482a05217b09bf3e472b2
Yuliya Romashko <ramashkay@gmail.com>
2023-06-08 13:38:27 +0300

    Removed: Creation of service beans in ServiceConfig (user-service).

--- user-service/src/main/java/by/it_academy/user/config/ServiceConfig.java
+++ user-service/src/main/java/by/it_academy/user/config/ServiceConfig.java
@@ -1,17 +1,7 @@
 package by.it_academy.user.config;
 
-import by.it_academy.user.repositories.api.AuthEntityRepository;
-import by.it_academy.user.repositories.api.UserEntityRepository;
-import by.it_academy.user.repositories.api.VerificationCodeEntityRepository;
-import by.it_academy.user.service.AuthenticationService;
-import by.it_academy.user.service.UserService;
-import by.it_academy.user.service.VerificationService;
-import by.it_academy.user.service.api.IAuthenticationService;
-import by.it_academy.user.service.api.IUserService;
-import by.it_academy.user.service.api.IVerificationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -21,25 +11,5 @@ public class ServiceConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-    @Bean
-    public IAuthenticationService authenticationService(AuthEntityRepository authEntityRepository,
-                                                        IUserService userService,
-                                                        IVerificationService verificationService,
-                                                        ConversionService conversionService,
-                                                        PasswordEncoder passwordEncoder){
-        return new AuthenticationService(authEntityRepository, userService, verificationService, conversionService, passwordEncoder);
-    }
-
-    @Bean
-    public IVerificationService verificationService(VerificationCodeEntityRepository verificationCodeEntityRepository) {
-        return new VerificationService(verificationCodeEntityRepository);
-    }
-
-    @Bean
-    public IUserService userService(UserEntityRepository userEntityRepository,
-                                    ConversionService conversionService,
-                                    PasswordEncoder passwordEncoder) {
-        return new UserService(userEntityRepository, conversionService, passwordEncoder);
     }
 }
