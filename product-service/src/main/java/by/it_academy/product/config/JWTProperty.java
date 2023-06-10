2f16327be9906a38b4aa721932234c773d32c057
Yuliya Romashko <ramashkay@gmail.com>
2023-06-10 18:22:10 +0300

    Added: lombok's annotations to JWTProperty (product-service)

--- product-service/src/main/java/by/it_academy/product/config/JWTProperty.java
+++ product-service/src/main/java/by/it_academy/product/config/JWTProperty.java
@@ -1,26 +1,14 @@
 package by.it_academy.product.config;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-
+@Getter
+@Setter
+@NoArgsConstructor
 @ConfigurationProperties(prefix = "jwt")
 public class JWTProperty {
     private String secret;
     private String issuer;
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public String getIssuer() {
-        return issuer;
-    }
-
-    public void setIssuer(String issuer) {
-        this.issuer = issuer;
-    }
-
 }
