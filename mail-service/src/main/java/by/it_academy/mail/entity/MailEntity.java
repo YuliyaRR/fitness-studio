6c115aede11011fed31be023f2a508ea0c5e99ca
Yuliya Romashko <ramashkay@gmail.com>
2023-06-11 23:19:32 +0300

    Added: lombok's annotations to entity (mail-service)

--- mail-service/src/main/java/by/it_academy/mail/entity/MailEntity.java
+++ mail-service/src/main/java/by/it_academy/mail/entity/MailEntity.java
@@ -2,15 +2,22 @@ package by.it_academy.mail.entity;
 
 import by.it_academy.mail.core.mail.MailTheme;
 import jakarta.persistence.*;
+import lombok.*;
 
-import java.util.Objects;
 import java.util.UUID;
-
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "mails", schema = "app")
 public class MailEntity {
     @Id
+    @EqualsAndHashCode.Include
     private UUID uuid;
+    @EqualsAndHashCode.Include
     private String message;
     private String email;
     @Enumerated(EnumType.STRING)
@@ -21,88 +28,4 @@ public class MailEntity {
     private int attemptOfSending;
     @Column(name = "limit_of_attempts")
     private int limitOfAttempts;
-
-    public MailEntity() {
-    }
-
-    public MailEntity(UUID uuid, String message,
-                      String email, MailTheme theme, boolean isSend,
-                      int attemptOfSending, int limitOfAttempts) {
-        this.uuid = uuid;
-        this.message = message;
-        this.email = email;
-        this.theme = theme;
-        this.isSend = isSend;
-        this.attemptOfSending = attemptOfSending;
-        this.limitOfAttempts = limitOfAttempts;
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(UUID uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public boolean isSend() {
-        return isSend;
-    }
-
-    public void setSend(boolean send) {
-        isSend = send;
-    }
-
-    public int getAttemptOfSending() {
-        return attemptOfSending;
-    }
-
-    public void setAttemptOfSending(int attemptOfSending) {
-        this.attemptOfSending = attemptOfSending;
-    }
-
-    public int getLimitOfAttempts() {
-        return limitOfAttempts;
-    }
-
-    public void setLimitOfAttempts(int limitOfAttempts) {
-        this.limitOfAttempts = limitOfAttempts;
-    }
-
-    public MailTheme getTheme() {
-        return theme;
-    }
-
-    public void setTheme(MailTheme theme) {
-        this.theme = theme;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        MailEntity that = (MailEntity) o;
-        return Objects.equals(uuid, that.uuid);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(uuid);
-    }
 }
