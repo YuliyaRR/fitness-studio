71e21f497c1647286148c72c807dc2c86da65e62
Yuliya Romashko <ramashkay@gmail.com>
2023-06-11 23:30:25 +0300

    Added: annotations for execution of asynchronous method and scheduled task (mail-service)

--- mail-service/src/main/java/by/it_academy/mail/MailApplication.java
+++ mail-service/src/main/java/by/it_academy/mail/MailApplication.java
@@ -2,7 +2,11 @@ package by.it_academy.mail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableAsync
 @SpringBootApplication
 public class MailApplication {
 
