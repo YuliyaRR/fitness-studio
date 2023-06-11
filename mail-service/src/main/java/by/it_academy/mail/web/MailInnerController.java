77af754319a1fff29d6770b462c6682211caf6b2
Yuliya Romashko <ramashkay@gmail.com>
2023-06-11 23:14:02 +0300

    Added: lombok's annotation to controller (mail-service)

--- mail-service/src/main/java/by/it_academy/mail/web/MailInnerController.java
+++ mail-service/src/main/java/by/it_academy/mail/web/MailInnerController.java
@@ -3,16 +3,13 @@ package by.it_academy.mail.web;
 import by.it_academy.mail.core.mail.EmailDetails;
 import by.it_academy.mail.service.api.IMailService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(path = "/mails")
+@RequiredArgsConstructor
 public class MailInnerController {
     private final IMailService mailService;
-
-    public MailInnerController(IMailService mailService) {
-        this.mailService = mailService;
-    }
-
     @RequestMapping(method = RequestMethod.POST)
     public void saveMail(@Valid @RequestBody EmailDetails emailDetails) {
         mailService.save(emailDetails);
