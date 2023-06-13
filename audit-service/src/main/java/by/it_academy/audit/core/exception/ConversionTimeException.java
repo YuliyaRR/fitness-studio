575afbc3c6c3a5568be9da9bcab13373c4efbee4
Yuliya Romashko <ramashkay@gmail.com>
2023-06-13 23:35:30 +0300

    Added: lombok's annotations to custom's exceptions; Deleted: unnecessary constructors(audit-service)

--- audit-service/src/main/java/by/it_academy/audit/core/exception/ConversionTimeException.java
+++ audit-service/src/main/java/by/it_academy/audit/core/exception/ConversionTimeException.java
@@ -1,11 +1,7 @@
 package by.it_academy.audit.core.exception;
 public class ConversionTimeException extends RuntimeException {
-
     public ConversionTimeException(String message) {
         super(message);
-    }
-
-    public ConversionTimeException() {
     }
 
 }
--- audit-service/src/main/java/by/it_academy/audit/core/exception/InvalidInputServiceSingleException.java
+++ audit-service/src/main/java/by/it_academy/audit/core/exception/InvalidInputServiceSingleException.java
@@ -1,28 +1,14 @@
 package by.it_academy.audit.core.exception;
 
 import by.it_academy.audit.core.dto.error.ErrorCode;
+import lombok.Getter;
 
+@Getter
 public class InvalidInputServiceSingleException extends IllegalArgumentException{
     private ErrorCode errorCode;
-
-    public InvalidInputServiceSingleException(String s) {
-        super(s);
-    }
 
     public InvalidInputServiceSingleException(String s, ErrorCode errorCode) {
         super(s);
         this.errorCode = errorCode;
-    }
-
-    public InvalidInputServiceSingleException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public InvalidInputServiceSingleException(Throwable cause) {
-        super(cause);
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 }
